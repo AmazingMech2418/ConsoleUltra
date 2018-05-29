f91ca75ca207174f4fbe31063b777b0ee24899dd
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-05-29 18:09:38 -0400

    Update total.js

--- total.js
+++ total.js
@@ -16,7 +16,9 @@ console.log("%c"+log,style);
 // warnings
 
 console.onOpen = function() {}
-console.setWarning = function (text,style) {var txt = text; var stl = style;console.onOpen = function(){console.style(txt,stl)};}
+var txt;
+var stl;
+console.setWarning = function (text,style) { txt = text;  stl = style;console.onOpen = function(){console.style(txt,stl)};}
 var aaa = 0;
 function interval() {
 if(aaa%2 === 0) {
