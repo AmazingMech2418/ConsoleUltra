72e7a47b0c13975e6a220b65578b51a88d072fdd
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-05-29 17:41:29 -0400

    Update images.js

--- images.js
+++ images.js
@@ -1,5 +1,6 @@
 console.img = function (image) {
 var i = new Image();
 i.onload = function(){console.log("%c"+"+","color:transparent;background: url("+image+"); background-repeat: no-repeat; padding: "+this.height+"px "+this.width+"px;   background-size: "+this.height+"px "+this.width+"px;");
-                     }i.src  = image;
+                     }
+i.src  = image;
 }
