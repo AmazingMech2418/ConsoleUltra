63c1cdc5c28e7a41b57ebb2279fb1b53bd1d091f
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-05-29 17:48:16 -0400

    Update images.js

--- images.js
+++ images.js
@@ -1,6 +1,6 @@
 console.img = function (image) {
 var i = new Image();
-i.onload = function(){console.log("%c"+"","color:transparent;background: url("+image+");  padding: "+this.height+"px "+this.width+"px;   background-size: "+this.height+"px "+this.width+"px;");
+i.onload = function(){console.log("%c"+"","color:transparent;background: url("+image+");  padding: "+this.height/2+"px "+this.width/2+"px;   background-size: "+this.height+"px "+this.width+"px;");
                      }
 i.src  = image;
 }
